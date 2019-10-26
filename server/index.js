b4f8afa92d44e82c6ee31cb306fa71a33483f488
max <mistrhell94@gmail.com>
2019-10-27 00:49:10 +0300

    mongo successfully connection from express

--- server/index.js
+++ server/index.js
@@ -2,6 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3300
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const mongoUrl = 'mongodb://mongo:27017'
+
+app.get('/', (req, res) => {
+
+
+    MongoClient.connect(mongoUrl, (err, client) => {
+        console.log("Connected successfully to mongo server");
+        
+        const db = client.db('test');
+       
+
+
+        client.close();
+      });
+
+
+
+    return res.send('Hello World!!!!')
+})
+
+
+
+app.listen(port, () => console.log(`EXPRESS APP LISTEN ${port}!`))
